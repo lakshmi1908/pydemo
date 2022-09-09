fc0ddac4be9a1144daaf825aa2574b510c406901
lakshmi1908 <maha1982002@gmail.com>
2022-09-09 22:52:07 +0530

    first commit

---
+++ python1.py
@@ -0,0 +1,15 @@
+# Python program to  
+# string concatenation  
+  
+str1 = "Hello"  
+str2 = "JavaTpoint"  
+  
+# join() method is used to combine the strings  
+print("".join([str1, str2]))  
+  
+# join() method is used to combine  
+# the string with a separator Space(" ")  
+str3 = " ".join([str1, str2])  
+  
+print(str3)  
+
